a4778e3217eb7c9ac631c69b1395c86582fad05a
Dudduly <150224716+Dudduly@users.noreply.github.com>
2023-11-09 14:18:53 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+//.....many
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+console.log(null >0); // false//false
+console.log(null ==0);//false
+console.log(null >=0);//true
+
+
